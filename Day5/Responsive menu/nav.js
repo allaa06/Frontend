c85dda51fa88dcc9f79515963d060ae55fa290b4
allaa06 <allaarshakyan742@gmail.com>
2024-02-26 19:01:56 +0400

    m

---
+++ Day5/Responsive menu/nav.js
@@ -0,0 +1,19 @@
+var hamburger = document.querySelector("#hamburger");
+var nav = document.querySelector(".navMenu");
+var navLinks = document.querySelectorAll(".navMenu li");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+      hamburger.classList.toggle("toggle");
+  });
+
+  Array.from(navLinks).forEach((li) =>
+  li.addEventListener("click", () => {
+    if (hamburger.classList.contains("toggle")) {
+      hamburger.classList.remove("toggle");
+    }
+    if (nav.classList.contains("nav-active")) {
+      nav.classList.remove("nav-active");
+    }
+  })
+);
